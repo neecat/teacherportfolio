9cb40cae20f795a8b81976eabe128b2235c76c18
Nathanial Karahalis <n.karahalis@gmail.com>
2023-03-24 13:49:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -21,6 +21,26 @@ function handleNavClicks() {
     });
   });
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll("#nav-bar li a");
+  const contentSections = document.querySelectorAll(".content");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const targetId = this.getAttribute("href").slice(1);
+      const targetContent = document.getElementById(targetId);
+
+      contentSections.forEach((section) => {
+        section.classList.remove("visible");
+        section.classList.remove("fade");
+      });
+
+      targetContent.classList.add("visible");
+      targetContent.classList.add("fade");
+    });
+  });
+});
 
 
 
